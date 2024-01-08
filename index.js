d18e705c63ab525d4bc2d4006b4bc06fcd933ee9
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-08 16:04:44 -0400

    aprendendo javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+let celular = function(){
+
+    this.cor = "prata";
+
+    this.ligar = function() 
+    {
+
+        console.log("uma ligação");
+        return ('ligando')
+
+    }
+
+}
+
+let objeto = new celular();
+
+console.log(objeto.ligar());
---
+++ index2.js
@@ -0,0 +1,5 @@
+let today = new Date();
+console.log(today.toLocaleDateString());
+ /* forma de mostrar a data atual de acordo com a região onde o usuário está acessando o sistema */
+
+ /* Uma class é um conjunto de aributos e métodos */
