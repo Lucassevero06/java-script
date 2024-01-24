9ade92858882f6b282ee2e932c2f33613d0dd65c
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-24 16:13:56 -0400

    começando do zero

--- index2.js
+++
@@ -1,5 +0,0 @@
-let today = new Date();
-console.log(today.toLocaleDateString());
- /* forma de mostrar a data atual de acordo com a região onde o usuário está acessando o sistema */
-
- /* Uma class é um conjunto de aributos e métodos */
