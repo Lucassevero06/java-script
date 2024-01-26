370927931dbfa972b3e33351e816924dc2797615
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-26 19:45:23 -0400

    praticando o if else com alterações no DOM

---
+++ aula-11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {  //condição simples tem apenas o if
+    console.log("Voce ultrapassou a velocidade permitida, MULTADO!")
+} else { //condição composta contém if e else
+    console.log('Voce está na velocidade adequada para essa via, siga em frente e dirija sempre usando cinto de segurança')
+}
---
+++ aula-11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'BRASIL'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'BRASIL') {
+    console.log('Voce é brasileiro!')
+} else {
+    console.log('Voce é estrangeiro')
+}
