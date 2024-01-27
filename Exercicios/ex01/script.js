db16c83aed8c08ef21187b1dea175a454a39e937
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-27 17:27:42 -0400

    código funcionando incirvelmente

--- Exercicios/ex01/script.js
+++ Exercicios/ex01/script.js
@@ -2,16 +2,18 @@ function carregar() {
     var msg = document.getElementById('msg');
     var img = document.getElementById('imagemHorario');
     var data = new Date();
-    //var hora = data.getHours();
-    var hora = 24
+    var hora = data.getHours();
     msg.innerHTML = `<p>Agora são <strong>${hora} horas</strong>!</p>`;
 
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/dia.png';
+        document.body.style.backgroundColor = '#FED355'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/tarde.png';
+        document.body.style.backgroundColor = '#E99516'
     } else {
         img.src = 'imagens/noite.png';
+        document.body.style.backgroundColor = '#3f3f3ff5'
     }
 }
 
