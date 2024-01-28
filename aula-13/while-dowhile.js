7a9c0109953a1ebd64d4028bdec8605f20965001
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-28 14:04:01 -0400

    while e do while

---
+++ aula-13/while-dowhile.js
@@ -0,0 +1,26 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*Usando o while:
+
+var c = 1 
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+Sem usar o While:
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
