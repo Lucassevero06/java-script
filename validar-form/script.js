01daf689349e3ec239a6c0044810a4f8bb4baa56
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-01 17:43:07 -0400

    código ainda resolvido

--- validar-form/script.js
+++ validar-form/script.js
@@ -3,7 +3,7 @@ let enviar = document.querySelector("#btn")
 console.log(inputs, enviar)
 function validar() {
     if (inputs.values.length === 0) {
-        
+        // problema não resolvido
         alert("[ERROR] todos os campos são OBRIGATÓRIOS")
     }
 }
