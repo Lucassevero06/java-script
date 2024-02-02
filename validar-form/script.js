ac92c8df65a9aa1d2410fca9c0e9f0ba030c3125
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-02 13:40:13 -0400

    desistindo

--- validar-form/script.js
+++
@@ -1,9 +0,0 @@
-let inputs = document.querySelectorAll(".input")
-let enviar = document.querySelector("#btn")
-console.log(inputs, enviar)
-function validar() {
-    if (inputs.values.length === 0) {
-        // problema não resolvido
-        alert("[ERROR] todos os campos são OBRIGATÓRIOS")
-    }
-}
