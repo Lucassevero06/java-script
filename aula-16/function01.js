c024b6ec8cf2db975d8861cd6a6feeea02d2bc7f
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-01 20:57:12 -0400

    diversos tipos de functions

---
+++ aula-16/function01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+// pode ser resumido para :
+//console.log(parimpar(6))
+let res = parimpar(6)
+console.log(res)
