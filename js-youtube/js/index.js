1cf8e45d4a42063de381445800c9468be781731b
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-22 19:32:32 -0400

    funções

--- js-youtube/js/index.js
+++ js-youtube/js/index.js
@@ -10,4 +10,42 @@ pessoa=nome+" "+soma;
 
 document.getElementById("texto").innerHTML=pessoa;
 */
+//////////////////////////////////////////////////////////////////////////////////////////////
+/*
+function soma(valor1, valor2) {
+    return valor1 + valor2;
+};
 
+function realParaDolar(real, cotacaoDolar) {
+    return real * cotacaoDolar;
+};
+
+var valorReal = 7.89;
+var cotacao = 5.08;
+
+var total = realParaDolar(valorReal,cotacao);
+
+alert("O valor em Real é R$: "+valorReal+" o valor em dólar é US: "+total);
+*/
+
+/*
+function alertHello(){
+    alert("Coé mano véio!")
+};
+
+alertHello();
+*/
+
+/*
+function paraCelcius(valorF){
+    return (5/9) * (valorF-32);
+};
+
+var x = paraCelcius(50);
+
+alert("A temperatura é de "+x+" graus Celcius.");
+*/
+
+function minhaFuncao(){
+    var x = 2;
+};
