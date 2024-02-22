de154420b3ebaf5beb2afec3ada78a92df39b36a
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-22 13:35:50 -0400

    22/02/2024
    aprimorando os estudos em javascript

---
+++ js-youtube/js/index.js
@@ -0,0 +1,13 @@
+/*
+var nome, sobrenome, nomeCompleto, idade, soma;
+nome="Dimitri";
+sobrenome="Teixeira";
+idade=30;
+nomeCompleto=nome+" "+sobrenome;
+
+soma=idade+10
+pessoa=nome+" "+soma;
+
+document.getElementById("texto").innerHTML=pessoa;
+*/
+
