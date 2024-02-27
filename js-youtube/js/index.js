5c16307fb91569c4b8144cebf50071a127090271
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-02-26 20:57:07 -0400

    26/02/2024
    entendo um pouco mais sobre objetos, que são nada mais do que variáveis que contém diversos informações armazenadas.

--- js-youtube/js/index.js
+++ js-youtube/js/index.js
@@ -51,3 +51,18 @@ function minhaFuncao(){
     var x = 2;
 };
 */
+
+const carro = {
+    marca: "ford",
+    modelo: "ka",
+    ano: "2015",
+    placa: "ABC-1234",
+    buzina: function() {
+        alert("biiiiiiiiiiiiiiii")
+    },
+    completo: function() {
+        return "A marca é: "+this.marca+" e o modelo é: "+this.modelo
+    }
+};
+
+console.log(carro.completo());
